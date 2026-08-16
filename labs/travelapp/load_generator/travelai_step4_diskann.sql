5c6d872973094d9f3054194e247c38e9f1be36da
Sudhir Amin <sudhamin@amazon.com>
2026-08-15 20:12:10 -0400

    Fix vector index - Destinations only, note BIGINT limit

--- labs/travelapp/load_generator/travelai_step4_diskann.sql
+++ labs/travelapp/load_generator/travelai_step4_diskann.sql
@@ -1,17 +1,15 @@
 USE TravelAI;
 GO
 
--- Create vector index on destinations
+-- Create vector index on destinations (enables fast ANN search)
 CREATE VECTOR INDEX IX_Destinations_Vector
 ON Destinations(description_vector)
 WITH (METRIC = 'cosine');
 GO
 
--- Create vector index on document chunks
-CREATE VECTOR INDEX IX_DocumentChunks_Vector
-ON DocumentChunks(content_vector)
-WITH (METRIC = 'cosine');
-GO
+-- Note: DocumentChunks uses BIGINT primary key which requires INT for vector index.
+-- Vector search on DocumentChunks still works via brute-force VECTOR_DISTANCE
+-- (fast enough for <1000 rows). For large-scale, alter chunk_id to INT.
 
 -- Verify
 SELECT name, type_desc 
